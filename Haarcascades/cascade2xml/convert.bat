3e01cfb2d0455b10b0f8948884f13ed4045d0962
Anum Jamil <72625562+anumjami2020l@users.noreply.github.com>
2025-01-13 00:11:38 +0500

    Add files via upload

---
+++ Haarcascades/cascade2xml/convert.bat
@@ -0,0 +1,4 @@
+haarconv.exe data Carbatch_14Aug.xml 50 35
+rem data - catalogue containing cascades in catalogues from 0 to "n" -where "n" - number of stages calculated by haartraining tool
+rem output.xml - output file ready to use in test application
+rem 24 -24 parametres used in haartraining- width and heigth of training region
