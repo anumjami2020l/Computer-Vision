8f50a24764a3b3ec02bc841d9d7a245d46f7739c
Anum Jamil <72625562+anumjami2020l@users.noreply.github.com>
2025-01-13 00:17:05 +0500

    Add files via upload

---
+++ Haarcascades/test_recognition/start.bat
@@ -0,0 +1,11 @@
+facedetect.exe --cascade="Carbatch_14Aug.xml" DSC_0968.jpg -w 50 -h 35
+facedetect.exe --cascade="Carbatch_14Aug.xml" DSC_0969.jpg
+facedetect.exe --cascade="Carbatch_14Aug.xml" DSC_1098.jpg
+
+rem facedetect.exe --cascade="masters-classifier.xml" Test_trafic.avi
+
+rem facedetect.exe --cascade="dane.xml" 8093_4143055422410_242822173_n.jpg
+rem facedetect.exe --cascade="masters-classifier.xml" 55.bmp
+pause
+rem dane.xml - its our trained and converted to xml haar cascade
+rem "0" at the end of the line its says that we will be using camera as a source of images, Instead of camera You can put file name for egz [pic0.jpg] or movie [film.avi]
